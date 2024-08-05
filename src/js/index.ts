7df033e2a1733e2bf2c37e15ad96a941073b2ea8
Joris <joris.gallot18@gmail.com>
2024-08-05 22:09:41 +0200

    chore: export not only components type

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,6 +1,6 @@
-export type { default as Splide } from './components/Splide/Splide.vue';
-export type { default as SplideTrack } from './components/SplideTrack/SplideTrack.vue';
-export type { default as SplideSlide } from './components/SplideSlide/SplideSlide.vue';
+export { default as Splide } from './components/Splide/Splide.vue';
+export { default as SplideTrack } from './components/SplideTrack/SplideTrack.vue';
+export { default as SplideSlide } from './components/SplideSlide/SplideSlide.vue';
 
 export { VueSplide as default } from './plugin/plugin';
 export type { Options } from '@splidejs/splide';
