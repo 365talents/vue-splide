cd490a2a06ca879cda80b58743ae163784762d8a
Joris <joris.gallot18@gmail.com>
2024-08-06 17:39:08 +0200

    chore: set project as type module

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,9 @@
-module.exports = {
+import type { Config } from 'jest';
+
+export default {
   rootDir: './src',
   transform: {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   testEnvironment: 'jsdom',
-};
+} satisfies Config;
